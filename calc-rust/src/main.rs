1226917a8d95c4d899fd0a6513c673a66f1c675a
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-04-08 10:52:19 -0300

    Adicionada entrada de dados e atribuição de valores às variáveis

--- calc-rust/src/main.rs
+++ calc-rust/src/main.rs
@@ -10,10 +10,39 @@ fn main() {
     let mut padrao_cruz = String::new();
 
     println!("--- Calculadora de raios ---");
-    println!("Digite o ERD da roda: ");
 
-    io::stdin().read_line(&mut erd)
-        .expect("Falha ao ler entrada");
+    println!("Digite o ERD da roda: ");
+    io::stdin().read_line(&mut erd).expect("Falha ao ler entrada");
+    let erd: f32 = match erd.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Entrada inválida"),
+    };
+
+    println!("Digite o diâmetro do cubo da bicicleta: ");
+    io::stdin().read_line(&mut diam_cubo).expect("Falha ao ler entrada");
+    let diam_cubo: f32 = match diam_cubo.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Entrada inválida"),
+    };
+
+    println!("Digite o número de furos do cubo: ");
+    io::stdin().read_line(&mut num_furos).expect("Falha ao ler entrada");
+    let num_furos: f32 = match num_furos.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Entrada inválida"),
+    };
+
+    println!("Digite o padrão de variação: ");
+    io::stdin().read_line(&mut padrao_cruz).expect("Falha ao ler entrada");
+    let padrao_cruz: i32 = match padrao_cruz.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Entrada inválida")
+    };
+
+
 
     println!("ERD: {}", erd);
+    println!("Diâmetro do cubo: {}", diam_cubo);
+    println!("Número de furos do cubo: {}", num_furos);
+    println!("Padrão de cruzamento do aros: {}", padrao_cruz);
 }
