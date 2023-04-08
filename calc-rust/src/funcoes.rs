875dc64a1ffb723293d435fbd7a77ae3802d910a
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-04-08 11:30:32 -0300

    Adicionada função para calcular o fator de flange

--- calc-rust/src/funcoes.rs
+++ calc-rust/src/funcoes.rs
@@ -1,5 +1,7 @@
-pub fn fator_flange() {
-    println!("Função fator flange");
+pub fn fator_flange(num_furos: i32, padrao_cruz: f64) -> f64 {
+    let angulo = 360.0 / (num_furos as f64 * 0.5 * padrao_cruz);
+    let fator_flange = angulo.to_radians().cos();
+    fator_flange
 }
 
 pub fn camp_raio(){
